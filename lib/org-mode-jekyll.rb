e8d1396f4d7dea7620d4d2d1ed74586fa29d7dbd
Jared Hankle <jdhankle@gmail.com>
2015-03-21 01:13:13 -0400

    fix

--- lib/org-mode-jekyll.rb
+++ lib/org-mode-jekyll.rb
@@ -1,24 +1,26 @@
 require 'jekyll'
 require 'org-ruby'
 
-module Jekyll
-  module Converters
+module OrgModeJekyll
+  module Jekyll
+    module Converters
 
-    class Org < Converter
-      safe true
+      class Org < Converter
+        safe true
 
-      def matches(ext)
-        ext =~ /^\.org$/i
+        def matches(ext)
+          ext =~ /^\.org$/i
+        end
+
+        def output_ext(ext)
+          '.html'
+        end
+
+        def convert(content)
+          ::Orgmode::Parser.new(content).to_html
+        end
+
       end
-
-      def output_ext(ext)
-        '.html'
-      end
-
-      def convert(content)
-        ::Orgmode::Parser.new(content).to_html
-      end
-
     end
   end
 end
